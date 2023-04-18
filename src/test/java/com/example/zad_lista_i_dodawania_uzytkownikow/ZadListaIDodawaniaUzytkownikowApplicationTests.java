36c4337efe95f170b01499efc2dc85de0e12b858
Krzysiek Przystarz <frodson35@gmail.com>
2023-04-18 21:01:37 +0200

    zad

---
+++ src/test/java/com/example/zad_lista_i_dodawania_uzytkownikow/ZadListaIDodawaniaUzytkownikowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zad_lista_i_dodawania_uzytkownikow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadListaIDodawaniaUzytkownikowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
